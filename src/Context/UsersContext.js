338db1b5d56d534f43d4255513780bf5230353dd
amjad <aao001056485@gmail.com>
2021-02-20 15:28:42 -0500

    changed UserContext mock data

--- src/Context/UsersContext.js
+++ src/Context/UsersContext.js
@@ -6,18 +6,18 @@ export const UsersProvider = (props) => {
   const [users, setUsers] = useState(() => [
     {
       id: 1,
-      username: 'User 1',
-      password: 'password1',
+      username: 'User1',
+      password: 'Pass1',
     },
     {
       id: 2,
-      username: 'User 2',
-      password: 'password2',
+      username: 'User2',
+      password: 'Pass2',
     },
     {
       id: 3,
-      username: 'User 3',
-      password: 'password3',
+      username: 'User3',
+      password: 'Pass3',
     },
   ]);
   return (
