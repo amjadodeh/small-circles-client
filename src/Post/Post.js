ed0a994f313c4c200b4230c2dad19fee0c7eb510
amjad <aao001056485@gmail.com>
2021-02-20 16:39:54 -0500

    refactored PostList to use new Post component

---
+++ src/Post/Post.js
@@ -0,0 +1,7 @@
+import './Post.css';
+
+const Post = (props) => {
+  return <div className="post">{props.content}</div>;
+};
+
+export default Post;
--- src/PostList/PostList.js
+++ src/PostList/PostList.js
@@ -1,10 +1,13 @@
 import { useContext } from 'react';
 
 import { PostsContext } from '../Context/PostsContext';
+import { LoggedInContext } from '../Context/LoggedInContext';
+import Post from '../Post/Post';
 import './PostList.css';
 
 const PostList = (props) => {
   const [posts, setPosts] = useContext(PostsContext);
+  const [loggedIn, setLoggedIn] = useContext(LoggedInContext);
 
   if (props.search) {
     return posts.find((post) =>
@@ -12,21 +15,16 @@ const PostList = (props) => {
     )
       ? posts.map(
           (post) =>
-            post.content.toLowerCase().includes(props.search.toLowerCase()) && (
-              <div key={post.id} className="post">
-                {post.content}
-              </div>
-            )
+            post.content.toLowerCase().includes(props.search.toLowerCase()) &&
+            !post.private && <Post key={post.id} content={post.content} />
         )
       : 'nothing here';
     // } else if (props.user) { // future implementation
     //   return <div></div>;
   } else {
-    return posts.map((post) => (
-      <div key={post.id} className="post">
-        {post.content}
-      </div>
-    ));
+    return posts.map(
+      (post) => !post.private && <Post key={post.id} content={post.content} />
+    );
   }
 };
 
