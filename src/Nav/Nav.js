97784cf72604d11cef84c602e0b91d3826d60000
amjad <aao001056485@gmail.com>
2021-02-28 05:37:31 -0500

    added styling to Nav component

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,5 +1,8 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { BiHome, BiMessageSquare } from 'react-icons/bi';
+import { IoSearch } from 'react-icons/io5';
+import { RiAccountCircleLine } from 'react-icons/ri';
 
 import { LoggedInContext } from '../Context/LoggedInContext';
 import './Nav.css';
@@ -8,23 +11,52 @@ const Nav = () => {
   const [loggedIn, setLoggedIn] = useContext(LoggedInContext);
 
   return (
-    <nav className="nav">
-      <Link to="/home">
-        <button>home</button>
-      </Link>
-      <Link to="/explore">
-        <button>explore</button>
-      </Link>
-      <Link to="/add-post">
-        <button>add post</button>
-      </Link>
-      <Link to="/messages">
-        <button>messages</button>
-      </Link>
-      <Link to={`/account/${loggedIn.id}`}>
-        <button>account</button>
-      </Link>
-    </nav>
+    <>
+      <nav className="nav">
+        <NavLink
+          to="/home"
+          className="nav-btn-link"
+          activeClassName="nav-btn-active-link"
+        >
+          <button className="nav-btn">
+            <BiHome size="2em" title="Home" />
+          </button>
+        </NavLink>
+        <NavLink
+          to="/explore"
+          className="nav-btn-link"
+          activeClassName="nav-btn-active-link"
+        >
+          <button className="nav-btn">
+            <IoSearch size="2em" title="Explore" />
+          </button>
+        </NavLink>
+
+        <NavLink
+          to="/messages"
+          className="nav-btn-link"
+          activeClassName="nav-btn-active-link"
+        >
+          <button className="nav-btn">
+            <BiMessageSquare size="2em" title="Messages" />
+          </button>
+        </NavLink>
+        <NavLink
+          to={`/account/${loggedIn.id}`}
+          className="nav-btn-link"
+          activeClassName="nav-btn-active-link"
+        >
+          <button className="nav-btn">
+            <RiAccountCircleLine size="2em" title="Account" />
+          </button>
+        </NavLink>
+      </nav>
+      <NavLink to="/add-post">
+        <button className="nav-add-post-btn">
+          <BiHome size="2em" color="#fff" title="Add Post" />
+        </button>
+      </NavLink>
+    </>
   );
 };
 
