d66311c05bea6cca8c748c7bee5663a4ae395782
amjad <aao001056485@gmail.com>
2021-03-01 02:58:45 -0500

    added styling and set up public file

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,17 +1,15 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { BiData } from 'react-icons/bi';
+import { AiFillGithub, AiFillLock } from 'react-icons/ai';
 
 import SignUpForm from '../SignUpForm/SignUpForm';
 import LogInForm from '../LogInForm/LogInForm';
+import scLogoOrange from '../images/sc-logo-orange.png';
+import scFullWhite from '../images/sc-full-white.png';
 import './LandingPage.css';
 
 const LandingPage = () => {
-  const [showLearn, setShowLearn] = useState(false);
   const [newUser, setNewUser] = useState(false);
-
-  const handleClickLearn = () => {
-    setShowLearn((currentShowLearn) => !currentShowLearn);
-  };
 
   const handleClickChangeForm = () => {
     setNewUser((newUser) => !newUser);
@@ -19,43 +17,80 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page-container">
-      <div className="landing-page-content">
-        <h1>Small Circles</h1>
-        <p>Your data is your data.</p>
+      <div className="landing-top-section">
+        <div className="landing-page-img-div">
+          <img
+            className="landing-page-sc-logo-orange"
+            src={scLogoOrange}
+            width="60px"
+          />
+
+          <img
+            className="landing-page-sc-full-white"
+            src={scFullWhite}
+            width="300px"
+          />
+        </div>
 
         <div className="landing-form-container">
           {!newUser ? <LogInForm /> : <SignUpForm />}
+          <div>
+            {!newUser ? (
+              <div>
+                Don't have an account?{' '}
+                <button
+                  className="landing-change-form-btn"
+                  onClick={handleClickChangeForm}
+                >
+                  Sign up.
+                </button>
+              </div>
+            ) : (
+              <div>
+                Already have an account?
+                <button
+                  className="landing-change-form-btn"
+                  onClick={handleClickChangeForm}
+                >
+                  Log in.
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="landing-bottom-section">
+        <div className="landing-learn-more-div">
+          <header>
+            <h3>Your Data is Your Data</h3>
+          </header>
+          <BiData size="4em" color="#fff" />
+          <p>
+            Small Circles is a social platform focused on giving you control of
+            your data.
+          </p>
         </div>
 
-        <button onClick={handleClickLearn}>
-          Learn more about Small Circles
-        </button>
-        {showLearn && (
-          <div>
-            Small Circles is a social networking service focused on giving you
-            control of your data. With every post, you decide who can see your
-            content. You can make a private post that only you can see, a public
-            post to be seen by all other users from the main feed, or select
-            from your friend list or from your premade circle of friends to
-            share to! You can even make your account private so no one can even
-            know you have an account! Small Circles is free to use and fully
-            open source.
-          </div>
-        )}
+        <div className="landing-learn-more-div">
+          <header>
+            <h3>Giving You Control</h3>
+          </header>
+          <AiFillLock size="4em" color="#fff" />
+          <p>
+            With every post, you decide who can see your content. You can make a
+            private post that only you can see, a post to be seen by all your
+            friends, or select from your friend list to share to!
+          </p>
+        </div>
 
-        <hr className="landing-hr" />
-
-        {!newUser ? (
-          <div>
-            Don't have an account?{' '}
-            <button onClick={handleClickChangeForm}>Sign up.</button>
-          </div>
-        ) : (
-          <div>
-            Already have an account?{' '}
-            <button onClick={handleClickChangeForm}>Log in.</button>
-          </div>
-        )}
+        <div className="landing-learn-more-div">
+          <header>
+            <h3>Open Source</h3>
+          </header>
+          <AiFillGithub size="4em" color="#fff" />
+          <p>Small Circles is free to use and fully open source.</p>
+        </div>
       </div>
     </div>
   );
